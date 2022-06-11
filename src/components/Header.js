41bc7436ef4bd6d2d7f93a178a4f2b216092fe4b
Ayomikuolatunji <Ayomikuolatunji@gmail.com>
2022-06-11 19:59:10 +0100

    I am working on the page header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { navItems } from '../helpers/navtItems'
 
 const Header = () => {
   return (
@@ -10,24 +12,26 @@ const Header = () => {
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Task</span>
             </a>
         </div>
-            <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                <li>
-                    <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+        <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">   
+             <li>
+                {
+                    navItems.map((menuItem,index)=>{
+                        return (
+                          <Link to={menuItem.url}>
+                              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{menuItem.name}</a>
+                          </Link>
+                        )
+                    })
+                }
                 </li>
-                <li>
-                    <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                </li>
-                <li>
-                    <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                </li>
-            </ul>
-            <div>
-            <button class="relative inline-flex items-center justify-center text-sm font-medium text-gray-900 rounded-lg border-blue-500 border-2 p-3 hover:bg-blue-500 hover:text-white">
+        </ul>
+       <div>
+            <button class="relative inline-flex items-center justify-center text-sm font-medium text-gray-900 rounded-lg border-[blue] border-2 py-3 px-8 hover:bg-blue-600 hover:text-white">
                     Logout
                 </button>
-            </div>
         </div>
-     </nav>
+     </div>
+ </nav>
 
   )
 }
